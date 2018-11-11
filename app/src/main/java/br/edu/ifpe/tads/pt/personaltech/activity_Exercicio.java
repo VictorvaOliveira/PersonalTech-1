2ee9e0ac36ecc38c435933a3589a059b4b309756
Unknown <mbff1998@gmail.com>
2018-11-11 20:25:17 -0200

    Cronometro alterado e funcionando

--- app/src/main/java/br/edu/ifpe/tads/pt/personaltech/activity_Exercicio.java
+++ app/src/main/java/br/edu/ifpe/tads/pt/personaltech/activity_Exercicio.java
@@ -1,5 +1,6 @@
 package br.edu.ifpe.tads.pt.personaltech;
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.Chronometer;
 public class activity_Exercicio extends AppCompatActivity {
    private Chronometer cronometro1;
    private boolean running;
+   private long pauseOffSet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +20,7 @@ public class activity_Exercicio extends AppCompatActivity {
 
     public void StartChronometer1(View v){
         if(!running){
+            cronometro1.setBase(SystemClock.elapsedRealtime() -pauseOffSet);
             cronometro1.start();
             running=true;
         }
@@ -27,6 +30,7 @@ public class activity_Exercicio extends AppCompatActivity {
     public void StopChronometer(View v){
         if(running){
             cronometro1.stop();
+            pauseOffSet= SystemClock.elapsedRealtime() - cronometro1.getBase();
             running=false;
         }
 

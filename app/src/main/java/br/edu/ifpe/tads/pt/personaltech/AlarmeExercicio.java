45e7ac5fd6fab08c4d897edf2b32d376fe1ec20b
Unknown <mbff1998@gmail.com>
2018-11-23 20:33:08 -0200

    notificação

---
+++ app/src/main/java/br/edu/ifpe/tads/pt/personaltech/AlarmeExercicio.java
@@ -0,0 +1,35 @@
+package br.edu.ifpe.tads.pt.personaltech;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmeExercicio extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+// Crio o Intent que será associado com o toque na notificação
+// esse Intent irá lançar a aplicação novamente.
+        Intent newIntent = new Intent(context, MainActivity.class);
+        newIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP |
+                Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingNotificationIntent = PendingIntent.getActivity(
+                context, 0, newIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+// Contrói a notificação que será exibida ao usuário
+        Notification.Builder builder = new Notification.Builder(context);
+        builder.setSmallIcon(R.drawable.notificacaoimagem);
+        builder.setContentText("Bora exercitar!");
+        builder.setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 });
+        builder.setContentIntent(pendingNotificationIntent);
+        builder.setAutoCancel(true);
+        Notification notification = builder.build();
+// Obter o gerenciador de notificações do sistema e exibe a notificação
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification);
+    }
+}
--- app/src/main/java/br/edu/ifpe/tads/pt/personaltech/Configuracao.java
+++ app/src/main/java/br/edu/ifpe/tads/pt/personaltech/Configuracao.java
@@ -1,13 +1,49 @@
 package br.edu.ifpe.tads.pt.personaltech;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Configuracao extends AppCompatActivity {
 
+    TimePicker timePicker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracao);
+        timePicker = (TimePicker)findViewById(R.id.relogioConf);
+        Calendar calendar = Calendar.getInstance();
+
+
+        timePicker.setCurrentHour(calendar.get(Calendar.HOUR_OF_DAY));
+        timePicker.setCurrentMinute(calendar.get(Calendar.MINUTE));
+    }
+    public void scheduleAlarm(View V) {
+       System.out.print("timePicker "+timePicker);
+// Tempo atual mais 10 segundos (10 * 1000 milissegundos)
+        Long time = new GregorianCalendar().getTimeInMillis()+3*1000;
+        System.out.print("timePicker "+timePicker);
+// Cria Intent do alarm, que será recebido pela classe AlarmReceiver
+        Intent intentAlarm = new Intent(this, AlarmeExercicio.class);
+// Cria um PendingIntent, usado para fazer o broadcast do alarme
+        PendingIntent pendingAlarmIntent = PendingIntent.getBroadcast(
+                this, 1, intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT);
+//Obtem o gerenciador de alarmes do sistema
+        AlarmManager alarmManager =
+                (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+//Configura um alarme lançará o intent em 10 segundos
+        alarmManager.set(AlarmManager.RTC_WAKEUP, time, pendingAlarmIntent);
+        Toast.makeText(this, "Alarme agendado.", Toast.LENGTH_LONG).show();
+// Finaliza a atividade
+        this.finish();
     }
 }
